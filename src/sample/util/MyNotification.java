ed6cb7e27dc391eb485228b175f418df0cf70701
Espoir <216093805@mycput.ac.za>
2020-11-07 01:28:38 +0200

    Ready for group Testing

---
+++ src/sample/util/MyNotification.java
@@ -0,0 +1,19 @@
+package sample.util;
+
+import javafx.geometry.Pos;
+import javafx.util.Duration;
+import org.controlsfx.control.Notifications;
+
+public class MyNotification {
+        // this method returns a notification panel with your specified title and text
+
+        public static void createNotification(String title,String text){
+            Notifications.create()
+                    .title(title)
+                    .text(text) .darkStyle()
+                    .hideAfter(Duration.seconds(3))
+                    .position(Pos.CENTER)
+                    .show();
+        }
+
+}
